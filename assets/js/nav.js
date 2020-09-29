be719721350edd9195f3f034f4fd4f37db6d93fb
geminerald <zacdwarner@gmail.com>
2020-09-29 23:44:37 +0000

    had intro logo disappear behind nav on small screens

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,9 +2,11 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const logo = document.querySelector('.intro-header');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        logo.classList.toggle('background');
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
